4ecbf55b6a09f8c882cc23101d52900c7c68daa4
Sahil Singh <125428501+Sahil-Singh2002@users.noreply.github.com>
2023-06-11 15:39:04 +0100

    Analtical_and_Computational_3.py

--- Analtical_and_Computational_3.py
+++ Analtical_and_Computational_3.py
@@ -1,64 +1,58 @@
 import random
 import sympy
-import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.patches import Circle,Rectangle
-#________________________________________________________________________________
+from matplotlib.patches import Circle, Rectangle
+
 def estimate_pi_monte_carlo(Ntotal):
-    if type (Ntotal) != int or Ntotal <= 0:
-        raise ValueError
-#The code below is just giving the parameter of the squar to be between -1 and 1 for both height and the base
-    points = [(random.uniform(-1,1),random.uniform(-1,1)) for _ in range(Ntotal)]
-    return sympy.Rational(4*sum([1 for point in points if point[0]**2 +point[1]**2 <= 1]), Ntotal)    
-#________________________________________________________________________________
+    if not isinstance(Ntotal, int) or Ntotal <= 0:
+        raise ValueError("Ntotal must be a positive integer")
+    
+    points = [(random.uniform(-1,1), random.uniform(-1,1)) for _ in range(Ntotal)]
+    count = sum(1 for point in points if point[0]**2 + point[1]**2 <= 1)
+    return sympy.Rational(4 * count, Ntotal)    
+
 def draw_monte_carlo(Ntotal):
-    fig = plt.figure(figsize =(10,5))
-#---------------------------------------------------------------
-# Left Subplot for the actual areas of the circle and Rectangle     
-    ax = fig.add_subplot(121)
-    rect= Rectangle((-1,-1),2,2, color = "red")
-    cir = Circle((0,0),1,color="green")
-    ax.add_patch(rect)
-    ax.set_xlim(-1,1)
-    ax.set_ylim(-1,1)
-    ax.set_xlabel("x")
-    ax.set_ylabel("y")
-    ax.add_patch(cir)
-    ax = fig.add_subplot(122)
-# Estimatation of the Monti carlo aprroximation
-    x, y = [random.uniform(-1,1) for _ in range(Ntotal)], [random.uniform(-1,1) for _ in range(Ntotal)]
-    xy = zip(x,y)
-    colour = ["green" if a[0]**2 +a[1]**2 <= 1 else "red" for a in xy]
-    ax.scatter(x, y, color = colour)
-    ax.add_patch(Circle((0,0),1,color ="blue", fill=False))
-    ax.set_xlabel("x")    
-    ax.set_xlim(-1,1)
-    ax.set_ylabel("y")
-    ax.set_ylim(-1,1)
-#Plot the Two graphs
+    fig, axes = plt.subplots(1, 2, figsize=(10, 5))
+    
+    rect = Rectangle((-1, -1), 2, 2, color="red")
+    cir = Circle((0, 0), 1, color="green")
+    axes[0].add_patch(rect)
+    axes[0].set_xlim(-1, 1)
+    axes[0].set_ylim(-1, 1)
+    axes[0].set_xlabel("x")
+    axes[0].set_ylabel("y")
+    axes[0].add_patch(cir)
+    
+    x = [random.uniform(-1, 1) for _ in range(Ntotal)]
+    y = [random.uniform(-1, 1) for _ in range(Ntotal)]
+    colours = ["green" if x[i]**2 + y[i]**2 <= 1 else "red" for i in range(Ntotal)]
+    axes[1].scatter(x, y, color=colours)
+    axes[1].add_patch(Circle((0, 0), 1, color="blue", fill=False))
+    axes[1].set_xlabel("x")
+    axes[1].set_xlim(-1, 1)
+    axes[1].set_ylabel("y")
+    axes[1].set_ylim(-1, 1)
+    
     fig.suptitle("Monte Carlo Approximation of Pi")
     plt.show()
     fig.savefig('outputimage.png')
-# Call the plotting function
+
 draw_monte_carlo(250)
-#_______________________________________________________________________
+
 def estimate_pi_chudnovsky(n):
-# Validation of the input
-    if type(n) != int or n<= 0:
-        raise ValueError
-#Constants
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("n must be a positive integer")
+    
     a = sympy.Rational(13591409)
-    i = sympy.symbols('i')
     b = sympy.Rational(545140134)
     c = sympy.Rational(640320)
-# makes sure we have a higher precision, mostly that of 1,000 or above. I Chose 10**4 because its fun to more accurate
-#__________________________________________________________________________________________________________
-    #Simplify the whole function by spliting the function into two parts the numerator and denominator, makes the code more easy to observe.
-    num = sympy.sympify(((-1)**i)*(sympy.factorial(6*i))*(a+b*i))
-    den = sympy.sympify((sympy.factorial(3*i))*((sympy.factorial(i))**3)*c**((3*i)+sympy.Rational(3,2)))
-    #The function is bellow in the simplest form.       
-    The_Function = sympy.sympify(num/den)
-    The_summation = sympy.Sum(The_Function,(i,0,n))
-    #The aprox_tion is just a rough estimate of pi up to 10000 digits
-    aprox_tion = (sympy.Rational(12)*The_summation.doit()).evalf(10**4)
-    return sympy.Float(aprox_tion**-1,10**4)
+    
+    i = sympy.symbols('i')
+    num = (-1)**i * sympy.factorial(6*i) * (a + b*i)
+    den = sympy.factorial(3*i) * (sympy.factorial(i)**3) * c**((3*i) + sympy.Rational(3, 2))
+    
+    The_Function = num / den
+    The_summation = sympy.Sum(The_Function, (i, 0, n))
+    approximation = (sympy.Rational(12) * The_summation.doit()).evalf(10**4)
+    
+    return sympy.Float(approximation**-1, 10**4)
